d5f3ffb75a0942171b03a651d8d0cc1776aa4945
Ryu Ko <ryukocn2008@yahoo.co.jp>
2018-04-21 14:52:47 +0000

    Finished Ch01_01

--- Qt5C++GUIProgrammingCookbook/Ch01_01/build-MainProject-Desktop-Debug/ui_mainwindow.h
+++ Qt5C++GUIProgrammingCookbook/Ch01_01/build-MainProject-Desktop-Debug/ui_mainwindow.h
@@ -13,11 +13,14 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QComboBox>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenuBar>
-#include <QtWidgets/QStatusBar>
-#include <QtWidgets/QToolBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QRadioButton>
+#include <QtWidgets/QSlider>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -25,28 +28,73 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
-    QMenuBar *menuBar;
-    QToolBar *mainToolBar;
     QWidget *centralWidget;
-    QStatusBar *statusBar;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QLineEdit *lineEdit;
+    QComboBox *comboBox;
+    QSlider *horizontalSlider;
+    QRadioButton *radioButton;
+    QCheckBox *checkBox;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
         MainWindow->resize(400, 300);
-        menuBar = new QMenuBar(MainWindow);
-        menuBar->setObjectName(QStringLiteral("menuBar"));
-        MainWindow->setMenuBar(menuBar);
-        mainToolBar = new QToolBar(MainWindow);
-        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        MainWindow->addToolBar(mainToolBar);
+        MainWindow->setStyleSheet(QLatin1String("* {\n"
+"	background: qradialgradient(spread:pad, cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4, radius: 1.35, stop:0 #fff, stop:1 #888);\n"
+"	color: rgb(255, 255, 255); \n"
+"	border: 1px solid #ffffff;\n"
+"}\n"
+"\n"
+"QPushButton {\n"
+"	color: red;\n"
+"	border: 10px;\n"
+"	padding: 0 8px;\n"
+"	background: white;\n"
+"}\n"
+"\n"
+"QPushButton#pushButton_2 {\n"
+"	border: 1px solid red; \n"
+"	border-radius: 10px; \n"
+"}\n"
+"\n"
+"QPushButton#pushButton_3 {\n"
+"	border: 2px solid gray; \n"
+"	border-radius: 10px; \n"
+"	padding: 0 8px; \n"
+"	background: yellow;\n"
+"}"));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(21, 21, 71, 31));
+        pushButton_2 = new QPushButton(centralWidget);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        pushButton_2->setGeometry(QRect(105, 21, 73, 31));
+        pushButton_3 = new QPushButton(centralWidget);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+        pushButton_3->setGeometry(QRect(188, 21, 75, 31));
+        lineEdit = new QLineEdit(centralWidget);
+        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+        lineEdit->setGeometry(QRect(20, 60, 113, 26));
+        comboBox = new QComboBox(centralWidget);
+        comboBox->setObjectName(QStringLiteral("comboBox"));
+        comboBox->setGeometry(QRect(20, 90, 76, 28));
+        horizontalSlider = new QSlider(centralWidget);
+        horizontalSlider->setObjectName(QStringLiteral("horizontalSlider"));
+        horizontalSlider->setGeometry(QRect(20, 130, 160, 20));
+        horizontalSlider->setOrientation(Qt::Horizontal);
+        radioButton = new QRadioButton(centralWidget);
+        radioButton->setObjectName(QStringLiteral("radioButton"));
+        radioButton->setGeometry(QRect(20, 160, 86, 20));
+        checkBox = new QCheckBox(centralWidget);
+        checkBox->setObjectName(QStringLiteral("checkBox"));
+        checkBox->setGeometry(QRect(20, 190, 77, 20));
         MainWindow->setCentralWidget(centralWidget);
-        statusBar = new QStatusBar(MainWindow);
-        statusBar->setObjectName(QStringLiteral("statusBar"));
-        MainWindow->setStatusBar(statusBar);
 
         retranslateUi(MainWindow);
 
@@ -56,6 +104,11 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+        pushButton->setText(QApplication::translate("MainWindow", "PushButton", 0));
+        pushButton_2->setText(QApplication::translate("MainWindow", "PushButton", 0));
+        pushButton_3->setText(QApplication::translate("MainWindow", "PushButton", 0));
+        radioButton->setText(QApplication::translate("MainWindow", "RadioButton", 0));
+        checkBox->setText(QApplication::translate("MainWindow", "CheckBox", 0));
     } // retranslateUi
 
 };
